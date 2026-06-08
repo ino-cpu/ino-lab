56469da460c084ffcd76ddb1178b966742d7f7d1
ino-cpu <inostore25@gmail.com>
2026-06-08 10:01:31 +0700

    Update random-meme-presiden.js

--- api/random/random-meme-presiden.js
+++ api/random/random-meme-presiden.js
@@ -1,14 +1,16 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
+  // Set CORS headers agar frontend bisa akses
   res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
-  // Data meme presiden (contoh)
   const memes = [
-    { url: 'https://i.imgflip.com/8l0w4n.jpg', title: 'Jokowi Angkat Tangan' },
-    { url: 'https://i.imgflip.com/5i7o3y.jpg', title: 'Prabowo Gemoy' },
-    { url: 'https://i.imgflip.com/7k4m2n.jpg', title: 'Megawati Senyum' }
+    { url: 'https://i.imgflip.com/8l0w4n.jpg', title: 'Jokowi Angkat Tangan', caption: 'Pak Jokowi lagi semangat!' },
+    { url: 'https://i.imgflip.com/5i7o3y.jpg', title: 'Prabowo Gemoy', caption: 'Gemoy abis!' },
+    { url: 'https://i.imgflip.com/7k4m2n.jpg', title: 'Megawati Senyum', caption: 'Senyum khas ibu bangsa' }
   ];
-  const randomMeme = memes[Math.floor(Math.random() * memes.length)];
-
-  return res.status(200).json(randomMeme);
+  const random = memes[Math.floor(Math.random() * memes.length)];
+  res.status(200).json(random);
 }
